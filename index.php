97f8477e186f3a2e4a48b6237dbd3bab7d378807
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-19 09:45:19 -0400

    Separated admin index.php into seperate files.

--- index.php
+++ index.php
@@ -1,30 +1,21 @@
 <?php include("includes/header.php"); ?>
 
 
-        <div class="row">
+<div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+	<!-- Blog Entries Column -->
+	<div class="col-md-8">
 
+	</div>
 
-
-
-
-
-            </div>
+	<!-- Blog Sidebar Widgets Column -->
+	<div class="col-md-4">
 
 
+		  <?php include("includes/sidebar.php"); ?>
 
 
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
+	</div>
+<!-- /.row -->
 
-
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
-        </div>
-        <!-- /.row -->
-
-        <?php include("includes/footer.php"); ?>
+<?php include("includes/footer.php"); ?>
