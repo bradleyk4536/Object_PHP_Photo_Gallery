5eb76ca5888f0b1988128e21b8f2f101d491dddc
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-20 10:52:59 -0400

    Reformated user.php

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -1,18 +1,15 @@
 <?php
 	class User {
-
 		public $id;
 		public $username;
 		public $password;
 		public $first_name;
 		public $last_name;
-
 //		find and display all users in users table
 		public static function find_all_users(){
 //
 			return self::find_this_query("SELECT * FROM users");
 		}
-
 //		find a user from database using the id
 		public static function find_user_by_id($id) {
 //			make database global so we can use the query_db method in Database class
@@ -33,29 +30,21 @@
 			$the_object_array = array();
 //			get table info for each row in table
 			while($row = mysqli_fetch_array($result_set)) {
-
 				$the_object_array[] = self::instantation($row);
 			}
 			return $the_object_array;
 		}
-
-
 //		auto instantiation of user object
 		private static function instantation($the_record) {
-
 //			create object User
 			$the_object = new self;
-
 			foreach($the_record as $the_attribute => $value) {
-
 				if($the_object ->has_the_attribute($the_attribute)) {
-
 					$the_object->$the_attribute = $value;
 				}
 			}
 			return $the_object;
 		}
-
 		private function has_the_attribute($the_attribute) {
 //			get all the properties of a object/class
 			$object_properties = get_object_vars($this);
