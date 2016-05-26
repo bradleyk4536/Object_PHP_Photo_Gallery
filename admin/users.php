88483044a6afffb74c7cf221736471d151aa188f
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-26 06:30:43 -0400

    Added new users.php html structure

--- admin/users.php
+++ admin/users.php
@@ -1,5 +1,9 @@
 <?php include("includes/header.php"); ?>
 
+<?php if(!$session->is_signed_in()) { redirect("login.php"); } ?>
+<?php
+$users = User::find_all();
+?>
 <!-- Navigation -->
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 
@@ -20,22 +24,49 @@
 	<div id="page-wrapper">
 
 	<div class="container-fluid">
-
 		 <!-- Page Heading -->
 		 <div class="row">
 			  <div class="col-lg-12">
 					<h1 class="page-header">
-						 Users
+						 User Gallery
 						 <small>Subheading</small>
 					</h1>
-					<ol class="breadcrumb">
-						 <li>
-							  <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-						 </li>
-						 <li class="active">
-							  <i class="fa fa-file"></i> Blank Page
-						 </li>
-					</ol>
+					<div class="col-md-12">
+						<table class="table table-hover">
+							<thead>
+								<tr>
+									<th>Id</th>
+									<th>Photo</th>
+									<th>Username</th>
+									<th>First Name</th>
+									<th>Last Name</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach($users as $user) : ?>
+									<tr>
+										<td><?php echo $user->id; ?> </td>
+										<td><img class="admin-user-thumbnail" src="<?php echo $user->image; ?>" alt="">
+										</td>
+
+										<td><?php echo $user->username; ?>
+										<div class="action_link">
+											<a href="edit_user.php?id=<?php echo $user->id ?>">Edit</a>
+											<a href="#">View</a>
+											<a href="delete_user.php?id=<?php echo $user->id ?>">Delete</a>
+										</div>
+										</td>
+										<td><?php echo $user->first_name; ?> </td>
+										<td><?php echo $user->last_name; ?> </td>
+<!--										path in src attribute is from class method for dynamic paths-->
+
+									</tr>
+								<?php endforeach; ?>
+
+							</tbody>
+						</table>
+
+					</div>
 			  </div>
 		 </div>
 		 <!-- /.row -->
