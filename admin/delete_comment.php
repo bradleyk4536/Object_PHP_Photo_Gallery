e61e480b9771ef79298c6c2a9ccff768fd655890
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-27 09:43:47 -0400

    Added delete_comment.php to application

---
+++ admin/delete_comment.php
@@ -0,0 +1,21 @@
+<?php include("includes/init.php"); ?>
+
+<?php if(!$session->is_signed_in()) { redirect("login.php"); } ?>
+
+<?php
+//check to see if photo is there
+if(empty($_GET['id'])) {
+
+	redirect("comments.php");
+}
+//call photo class find_by_id method
+$comment = Comment::find_by_id($_GET['id']);
+
+//test to see if photo id was gotten, if so delete if not redirect back to photos.php
+if($comment) {
+	$comment->delete();
+	redirect("comments.php");
+} else {
+	redirect("comments.php");
+}
+?>
