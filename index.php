1b74114ea714753f3bf6034a93c5d1840db27f9b
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-29 07:09:46 -0400

    Added pagination to application (created Paginate class)

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php include("includes/header.php"); ?>
 
+<?php
+//	For pagination first set page variable
+	$page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+//	Set total photos per page
+	$items_per_page = 8;
+//	Get total photo count
+	$items_total_count = Photo::count_all();
+?>
 <?php $photos = Photo::find_all(); ?>
-
 <div class="row">
 	<!-- Blog Entries Column -->
 	<div class="col-md-12">
 	<div class="thumbnails row">
 <?php foreach($photos as $photo) : ?>
-
 		<div class="col-xs-6 col-md-3">
 			<a class="thumbnail" href="photo.php?id=<?php echo $photo->id ?>">
 				<img class="img-responsive home_page_photo" src="admin/<?php echo $photo->picture_path(); ?>" alt="">
-
 			</a>
-
 		</div>
-
-
 <?php endforeach; ?>
 	</div>
 	</div>
-
 	<!-- Blog Sidebar Widgets Column -->
-
 <!-- /.row -->
 
 <?php include("includes/footer.php"); ?>
